f594849965b79dbc8a54428f93e7bee0d43ba0bc
Parice Gautier <patrice@mac.com>
2021-11-08 18:21:22 -0800

    linting

--- internal/workers/job_handler.go
+++ internal/workers/job_handler.go
@@ -5,31 +5,60 @@ import (
 
 	"context"
 	"encoding/json"
+	"fmt"
 	log "github.com/sirupsen/logrus"
+	"net/http"
 )
 
-func (w *BackgroundWorker) ProcessJob(ctx context.Context, id, img, constraint, tagPrefix string) error {
-	image, err := fresh_container.NewImage(img, tagPrefix)
-	if err != nil {
+func (w *BackgroundWorker) _handleError(ctx context.Context, tagPrefix string, id string, img string, constraint string, action string, err error) {
+	log.WithFields(log.Fields{
+		"action":     action,
+		"id":         id,
+		"image":      img,
+		"constraint": constraint,
+		"tagPrefix":  tagPrefix,
+		"error":      err,
+	}).Error("worker.ProcessJob")
+	msg := struct {
+		Error string
+	}{
+		fmt.Sprint(err),
+	}
+	encodedResult, err1 := json.Marshal(msg)
+	if err1 != nil {
 		log.WithFields(log.Fields{
+			"action":     "_handleError:Marshal",
 			"id":         id,
 			"image":      img,
 			"constraint": constraint,
 			"tagPrefix":  tagPrefix,
 			"error":      err,
 		}).Error("worker.ProcessJob")
+		return
+	}
+	if err2 := w.db.SetEvaluation(id, encodedResult, fmt.Sprint(http.StatusInternalServerError)); err2 != nil {
+		log.WithFields(log.Fields{
+			"action":     "_handleError:SetEvaluation",
+			"id":         id,
+			"image":      img,
+			"constraint": constraint,
+			"tagPrefix":  tagPrefix,
+			"error":      err,
+		}).Error("worker.ProcessJob")
+	}
+
+}
+
+func (w *BackgroundWorker) ProcessJob(ctx context.Context, id, img, constraint, tagPrefix string) error {
+	image, err := fresh_container.NewImage(img, tagPrefix)
+	if err != nil {
+		w._handleError(ctx, tagPrefix, id, img, constraint, "NewImage", err)
 		return err
 	}
 
 	// reach to external registry to fetch tags
 	if err = image.FetchTags(ctx, w.config); err != nil {
-		log.WithFields(log.Fields{
-			"id":         id,
-			"image":      img,
-			"constraint": constraint,
-			"tagPrefix":  tagPrefix,
-			"error":      err,
-		}).Error("worker.ProcessJob")
+		w._handleError(ctx, tagPrefix, id, img, constraint, "FetchTags", err)
 		return err
 	}
 
@@ -39,13 +68,7 @@ func (w *BackgroundWorker) ProcessJob(ctx context.Context, id, img, constraint, 
 		tagsString = append(tagsString, tag.String())
 	}
 	if err = w.db.SetImageTags(image, tagsString); err != nil {
-		log.WithFields(log.Fields{
-			"id":         id,
-			"image":      img,
-			"constraint": constraint,
-			"tagPrefix":  tagPrefix,
-			"error":      err,
-		}).Error("worker.ProcessJob")
+		w._handleError(ctx, tagPrefix, id, img, constraint, "save_tags", err)
 		return err
 	} else {
 		log.WithFields(log.Fields{
@@ -60,36 +83,18 @@ func (w *BackgroundWorker) ProcessJob(ctx context.Context, id, img, constraint, 
 
 	evaluation, err := image.EvalUpgrade(constraint)
 	if err != nil {
-		log.WithFields(log.Fields{
-			"id":         id,
-			"image":      img,
-			"constraint": constraint,
-			"tagPrefix":  tagPrefix,
-			"error":      err,
-		}).Error("worker.ProcessJob")
+		w._handleError(ctx, tagPrefix, id, img, constraint, "EvalUpgrade", err)
 		return err
 	}
 
 	encodedResult, err := json.Marshal(evaluation)
 	if err != nil {
-		log.WithFields(log.Fields{
-			"id":         id,
-			"image":      img,
-			"constraint": constraint,
-			"tagPrefix":  tagPrefix,
-			"error":      err,
-		}).Error("worker.ProcessJob")
+		w._handleError(ctx, tagPrefix, id, img, constraint, "Marshal", err)
 		return err
 	}
 
-	if err = w.db.SetEvaluation(id, encodedResult); err != nil {
-		log.WithFields(log.Fields{
-			"id":         id,
-			"image":      img,
-			"constraint": constraint,
-			"tagPrefix":  tagPrefix,
-			"error":      err,
-		}).Error("worker.ProcessJob")
+	if err = w.db.SetEvaluation(id, encodedResult, ""); err != nil {
+		w._handleError(ctx, tagPrefix, id, img, constraint, "SetEvaluation", err)
 		return err
 	}
 
